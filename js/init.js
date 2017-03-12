4c3ef7218a486306c74f1319bfffe822dfe491a5
Romain Arnaud <arnaud.romain34@gmail.com>
2017-03-12 23:19:38 +0100

    Add some musics and images and change the anim
    * Added 10 pics
    * Added 4 songs
    * Change the movement of animation from 25 to 50 px

--- js/init.js
+++ js/init.js
@@ -25,46 +25,64 @@ var songs = [
     'build': {'url': 'assets/music/build_Finale.mp3', 'beatmap': '.....'},
     'loop': {'url': 'assets/music/loop_Finale.mp3', 'beatmap': 'x..xo...x...o...x..xo...x...o...x..xo...x...o...x..xo...x...oxoox..xo...x...o...x..xo...x...o...x..xo...x...o...x...o...x...oooo'}
   },
-  {'id': 1, 'name': 'Vexare - The Clockmaker', 'beat_length': 53.58,
-    'build': {'url': 'assets/music/build_TheClockmaker.mp3', 'beatmap': '+...................................................................................................................................'},
-    'loop': {'url': 'assets/music/loop_TheClockmaker.mp3', 'beatmap': 'o.......-...+...x...-----...-...o..:..:.o....:..x.......o...-...o...........o...x.....x.....x...o...............+...............o.......----o--.x.......-...-...o..:..:.o....:..x.......o.-.-.-.o...........o...x...............o.......o.......x...............:...:...:...:.+.x...-----...-...o..:..:.o....:..x.......o...-...o...........o...x.....x.....x...o...............+...............o...........----x.....-.-...-...o..:..:.o....:..x.......o.-.-.-.o...........o...x...............o.......o.......x.....x.....-...'}
-  },
-  {'id': 2, 'name': 'Imagine Dragons - Radioactive', 'beat_length': 219.66,
+  {'id': 1, 'name': 'Imagine Dragons - Radioactive', 'beat_length': 219.66,
     'build': null,
     'loop': {'url': 'assets/music/loop_Radioactive.mp3', 'beatmap': 'o...x.o.o...x.o.o...x...o...x.o.o...x.o.o...x.......x.......x...'}
   },
-  {'id': 3, 'name': 'Row Row Fight the Powah (RAGEFOXX & SLUTTT MIX)', 'beat_length': 119.01,
+  {'id': 2, 'name': 'Row Row Fight the Powah (RAGEFOXX & SLUTTT MIX)', 'beat_length': 119.01,
     'build': null,
     'loop': {'url': 'assets/music/loop_RowRow.mp3', 'beatmap': 'o...x...o...x...o...x...o...x...o...x...o...x...o...x...o...xxx.x...x...o...x...o...x...o...x...o...x...o...x...o...o...o...o...'}
   },
-  {'id': 4, 'name': 'Outlaw Star OST - Desire', 'beat_length': 138.81,
+  {'id': 3, 'name': 'Outlaw Star OST - Desire', 'beat_length': 138.81,
     'build': {'url': 'assets/music/build_Desire.mp3', 'beatmap': 'x.....x.x.x.xxx.'},
     'loop': {'url': 'assets/music/loop_Desire.mp3', 'beatmap': 'o...x...o.o.x.x...o.x...o.o.x...o...x...o.o.x.x...o.x...oo..x.x.o...x...o.o.x.x...o.x...oo..x.x.o...x...o.o.x.x...o.x...x...x.xx'}
   },
-  {'id': 5, 'name': 'The Bloody Beetroots - Out of Sight', 'beat_length': 108.61,
+  {'id': 4, 'name': 'The Bloody Beetroots - Out of Sight', 'beat_length': 108.61,
     'build': null,
     'loop': {'url': 'assets/music/loop_OutOfSight.mp3', 'beatmap': 'o.....oox.......o.o....ox.......o.....oox...o...o.o....ox.......o......ox.......o.o....ox.......o.....oox...o...o.o.....+.......'}
   },
-  {'id': 6, 'name': 'Buckethead - Smile Without a Face', 'beat_length': 106.97,
+  {'id': 5, 'name': 'Buckethead - Smile Without a Face', 'beat_length': 106.97,
     'build': {'url': 'assets/music/build_SmileWithoutAFace.mp3', 'beatmap': '..o.....-:x.......o.....-:x.......o.....-:x.......o.....-:x.......o.....-:x.......o.....-:x.x......'},
     'loop': {'url': 'assets/music/loop_SmileWithoutAFace.mp3', 'beatmap': 'o......ox.....o.o.......x.......o......ox.....o.o.....o.x.ooooooo......ox.....o.o.......x.......o......ox.....o.o.....o.x.oooooo'}
   },
-  {'id': 7, 'name': 'Crystal Castles - Courtship Dating', 'beat_length': 249.87,
+  {'id': 6, 'name': 'Crystal Castles - Courtship Dating', 'beat_length': 249.87,
     'build': null,
     'loop': {'url': 'assets/music/loop_CourtshipDate.mp3', 'beatmap': 'o...x.....o.x...o...x.....o.x...o...x.....o.x...o...x.....o.+...'}
   },
-  {'id': 8, 'name': 'Aphex Twin - Vordhosbn', 'beat_length': 88.11,
+  {'id': 7, 'name': 'Aphex Twin - Vordhosbn', 'beat_length': 88.11,
     'build': {'url': 'assets/music/build_Vordhosbn.mp3', 'beatmap': ':...x..------.......-.-----.+...............x..-.-.--.-.-.-.---..'},
     'loop': {'url': 'assets/music/loop_Vordhosbn.mp3', 'beatmap': 'o...x..---o.x...-.o.x------.x..-o.-.x.o..-.ox-.-----x-o-------o-o...x..---o.x...-.o.x------.x..-o.-.x.o..-.ox-.-----x-o-------o-o...x..---o.x...-.o.x------.x..-o.-.x.o..-.ox-.-----x-o-------o-o...x..---o.x...-.o.x------.x..-o.-.x.o..-.ox-.-----x-o-------o-o...x..---o.x...-.o.x------.x..-o.-.x.o..-.ox-.-----x-o-------o-o...x..---o.x...-.o.x------.x..-o.-.x.o..-.ox-.-----x-o-------o-'}
   },
-  {'id': 9, 'name': 'Culprate - Orange Sunrise, Sunset', 'beat_length': 129.22,
+  {'id': 8, 'name': 'Culprate - Orange Sunrise, Sunset', 'beat_length': 129.22,
     'build': null,
     'loop': {'url': 'assets/music/loop_Orange.mp3', 'beatmap': 'o.o.x..o.x.x....o..ox......x....o...x....o.x....o...x....o.xx...o.o.x..o.x.x....o..ox......x....o...x....o.x...xo...x....o.xx...'}
   },
-  {'id': 10, 'name': 'Hyper - Spoiler', 'beat_length': 83.38,
+  {'id': 9, 'name': 'Hyper - Spoiler', 'beat_length': 83.38,
     'build': null,
     'loop': {'url': 'assets/music/loop_Spoiler.mp3', 'beatmap': 'o+......x+......o+......x+......o+......x+......o+......x+......o+......x+......o+......x+......o+......x+......o+..............o+......x+......o+......x+......o+......x+......o+......x+......o+......x+......o+......x+......o---------------o+......x.......'}
-  }
+  },
+  {'id': 10, 'name': 'DJ Fresh - Kryptonite', 'beat_length': 170.44,
+    'build': null,
+    'loop': {'url': 'assets/music/loop_Kryptonite.mp3', 'beatmap': 'o.x..ox.o.x..ox.o.x..ox.o.x..ox.o.x..xx.o.x..xx.o.x..xx.o.x..xx.o.x..ox.o.x..ox.o.x..ox.o.x..ox.o.x..xx.o.x..xx.o.xx.xx.o.x..xx.'}
+  },
+  {'id': 11, 'name': 'STS9 - Beyond Right Now (Glitch Mob Remix)', 'beat_length': 78.90,
+    'build': null,
+    'loop': {'url': 'assets/music/loop_BeyondRightNow.mp3', 'beatmap': 'o.......x...o.......o...xxxxx...o.o.....x...o.......o...xxxxx...o.......x...o.......o...xxxxx...o.......x...o...o.......+.......o.......x...o.......o...xxxxx...o.o.....x...o...-.-.o.-.xxx+x+x+o.......x...o.......o...xxxxx...o.......x...o...o...o.o.+.......'}
+  },
+  {'id': 12, 'name': 'Kanye West - Hold My Liquor', 'beat_length': 140.12,
+    'build': null,
+    'loop': {'url': 'assets/music/loop_HoldMyLiquor.mp3', 'beatmap': 'o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...o...............+...'}
+  },
+  {'id': 13, 'name': 'Savant - Heart', 'beat_length': 117.18,
+    'build': {'url': 'assets/music/build_Heart.mp3', 'beatmap': 'o....'},
+    'loop': {'url': 'assets/music/loop_Heart.mp3', 'beatmap': 'o...x...o...x...o...x...o...x...o...x...o...x...o...x...o...x.-.o...x...o...x...o...x...o...x...o...x...o...x...o...x...o...xx-.'}
+  },
+
+
+  {'id': 14, 'name': 'Vexare - The Clockmaker', 'beat_length': 53.58,
+    'build': {'url': 'assets/music/build_TheClockmaker.mp3', 'beatmap': '+...................................................................................................................................'},
+    'loop': {'url': 'assets/music/loop_TheClockmaker.mp3', 'beatmap': 'o.......-...+...x...-----...-...o..:..:.o....:..x.......o...-...o...........o...x.....x.....x...o...............+...............o.......----o--.x.......-...-...o..:..:.o....:..x.......o.-.-.-.o...........o...x...............o.......o.......x...............:...:...:...:.+.x...-----...-...o..:..:.o....:..x.......o...-...o...........o...x.....x.....x...o...............+...............o...........----x.....-.-...-...o..:..:.o....:..x.......o.-.-.-.o...........o...x...............o.......o.......x.....x.....-...'}
+  },
 ];
 
 /*
@@ -113,7 +131,18 @@ var pictures = [
   {'id': 29, 'name': 'Hanako'},
   {'id': 30, 'name': 'Haruhi'},
   {'id': 31, 'name': 'Haruhi_Fujioka'},
-  {'id': 32, 'name': 'Megumi'},
+  {'id': 32, 'name': 'Haruko'},
+  {'id': 33, 'name': 'Hitoha'},
+  {'id': 34, 'name': 'Holo'},
+  {'id': 35, 'name': 'Homura'},
+  {'id': 36, 'name': 'Hyatt'},
+  {'id': 37, 'name': 'Ibara'},
+  {'id': 38, 'name': 'Ika'},
+  {'id': 39, 'name': 'Kagami'},
+  {'id': 40, 'name': 'Kanade'},
+  {'id': 41, 'name': 'Kanoe'},
+
+  {'id': 42, 'name': 'Megumi'},
 ];
 
 
